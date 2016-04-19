300eecf8d494b1480baa1349f308e6328d031bd9
Maximilian Wilhelm <max@rfc2324.org>
2016-04-19 21:34:02 +0200

    Add own simple salt module.
    Signed-off-by: Maximilian Wilhelm <max@rfc2324.org>

---
+++ _modules/ffho.py
@@ -0,0 +1,7 @@
+import re
+
+def re_replace (pattern, replacement, string):
+	return re.sub (pattern, replacement, string)
+
+def re_search (pattern, string, flags = 0):
+	return re.search (pattern, string, flags)
