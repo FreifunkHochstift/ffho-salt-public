1b018c205c96d626a8e59ef72dbd6fe14333246e
salt-master <hilfe@ffmuc.net>
2020-11-06 16:09:51 +0100

    module:wireguard_v6: Cleanup

--- _modules/wireguard_v6.py
+++ _modules/wireguard_v6.py
@@ -1,31 +1,7 @@
 #!/usr/bin/python
-import requests
-import logging
 import hashlib
-import ipaddress
-import re
+from salt.util.network import mac2eui64
 from textwrap import wrap
-log = logging.getLogger(__name__)
-
-def mac2eui64(mac, prefix=None):
-    '''
-    Convert a MAC address to a EUI64 address
-    or, with prefix provided, a full IPv6 address
-    '''
-    # http://tools.ietf.org/html/rfc4291#section-2.5.1
-    eui64 = re.sub(r'[.:-]', '', mac).lower()
-    eui64 = eui64[0:6] + 'fffe' + eui64[6:]
-    eui64 = hex(int(eui64[0:2], 16) ^ 2)[2:].zfill(2) + eui64[2:]
-
-    if prefix is None:
-        return ':'.join(re.findall(r'.{4}', eui64))
-    else:
-        try:
-            net = ipaddress.ip_network(prefix, strict=False)
-            euil = int('0x{0}'.format(eui64), 16)
-            return str(net[euil])
-        except Exception as e:  # pylint: disable=bare-except
-            return e
 
 def generate(pubkey):
     m = hashlib.md5()
