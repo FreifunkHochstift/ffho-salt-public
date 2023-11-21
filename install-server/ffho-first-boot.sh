a1ebdff8f75fdb5be20ad063966ad509dfe1f74e
Maximilian Wilhelm <max@sdn.clinic>
2023-11-21 05:11:09 +0100

    install-server: Update first-boot script
     * Explicitly state that key has been accepted on master
     * Back up SSH keys, just in case
     * Show IP addresses of the system, to SSH into it
    
    Signed-off-by: Maximilian Wilhelm <max@sdn.clinic>

--- install-server/ffho-first-boot.sh
+++ install-server/ffho-first-boot.sh
@@ -9,6 +9,9 @@ while ! salt-call test.ping >/dev/null 2>&1; do
 	sleep 10
 done
 
+echo "Looks like you did, cool, let's get started!"
+echo
+
 ################################################################################
 #                           Set up screeen and SSH                             #
 ################################################################################
@@ -18,6 +21,19 @@ salt-call saltutil.sync_all
 
 echo "Configuring screen and SSH..."
 salt-call state.apply screen,ssh
+
+echo "Backing up SSH keys..."
+cp -a /etc/ssh /opt
+
+cat << EOF
+SSH configured, you should now be able to SSH into this device (as root).
+
+EOF
+
+ip -br a
+
+echo
+echo
 
 echo "Running highstate..."
 salt-call state.highstate
